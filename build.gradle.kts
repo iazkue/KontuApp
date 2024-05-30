bb01894f2d43303b5dc4cbec64ebd7cf66494d01
iazkue <azkue.153882@e.unavarra.es>
2024-05-30 21:42:44 +0200

    1.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:8.0.2")
+        classpath ("com.android.tools.build:gradle:8.2.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
         classpath ("com.google.gms:google-services:4.3.15")
         // Otras dependencias del classpath
